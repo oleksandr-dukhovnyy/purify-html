67fe15b5b2a466df3e6f180072576494c33b6cf5
Aleksandr Dukhovnyy <script@vip-person.net>
2022-12-16 16:09:09 +0200

    v1.2.0

---
+++ tests/setParser.test.js
@@ -0,0 +1,80 @@
+import Sanitizer, { setParser } from '@/core.js';
+import 'core-js/modules/web.structured-clone';
+
+describe('Sanitizer', () => {
+  test('normal setParser', () => {
+    const str = 'some string';
+    let strFromParser = null;
+
+    let createdDiv;
+    let divFromParser = null;
+
+    const customParser = {
+      parse(string) {
+        const div = document.createElement('div');
+        createdDiv = div;
+
+        strFromParser = string;
+
+        return div;
+      },
+      stringify(el) {
+        divFromParser = el;
+        return el.innerHTML;
+      },
+    };
+
+    const setUpCode = setParser(customParser);
+
+    const sanitizer = new Sanitizer();
+    sanitizer.sanitize(str);
+
+    expect(str).toEqual(strFromParser);
+    expect(createdDiv).toEqual(divFromParser);
+    expect(setUpCode).toEqual(1);
+  });
+
+  test('no .parse method', () => {
+    const str = 'some string';
+    let called = false;
+
+    const customParser = {
+      // parse() {},
+      stringify(el) {
+        called = true;
+        return el.innerHTML;
+      },
+    };
+
+    const setUpCode = setParser(customParser);
+
+    const sanitizer = new Sanitizer();
+    sanitizer.sanitize(str);
+
+    expect(called).toEqual(false);
+    expect(setUpCode).toEqual(0);
+  });
+
+  test('no .stringify method', () => {
+    const str = 'some string';
+    let called = false;
+
+    const customParser = {
+      parse() {
+        const div = document.createElement('div');
+        called = true;
+
+        return div;
+      },
+      // stringify() {},
+    };
+
+    const setUpCode = setParser(customParser);
+
+    const sanitizer = new Sanitizer();
+    sanitizer.sanitize(str);
+
+    expect(called).toEqual(false);
+    expect(setUpCode).toEqual(0);
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  // entry: {
-  //   esm: './lib/index.esm.js',
-  //   browser: './lib/index.browser.js',
-  // },
-  // devtool: false,
-  // output: {
-  //   path: path.resolve(__dirname, 'dist'),
-  //   filename: `index.[name].js`,
-  //   library: 'PurifyHTML',
-  //   libraryTarget: 'commonjs',
-  // },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.js$/,
-  //       exclude: /(node_modules|bower_components)/,
-  //       use: {
-  //         loader: 'babel-loader',
-  //         options: {
-  //           presets: ['@babel/preset-env'],
-  //           plugins: ['@babel/plugin-transform-runtime'],
-  //         },
-  //       },
-  //     },
-  //   ],
-  // },
+  entry: {
+    esm: './lib/index.esm.js',
+    browser: './lib/index.browser.js',
+  },
+  devtool: false,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `index.[name].js`,
+    library: 'PurifyHTML',
+    libraryTarget: 'umd',
+    clean: true,
+  },
+  module: {
+    rules: [
+      // use babel-loader only in *.browser.js build
+      {
+        test: /\.browser\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
 };
